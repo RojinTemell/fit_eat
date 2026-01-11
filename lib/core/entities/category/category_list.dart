af546d248b0414cc6bd6f9c26de85397c5a956ac
RojinTemell <rojintemel02@gmail.com>
2026-01-11 20:14:30 +0300

    category list

---
+++ lib/core/entities/category/category_list.dart
@@ -0,0 +1,8 @@
+import 'category_model.dart';
+
+List<Category> categoryList = [
+  Category(id: "breakfast", title: "Kahvaltı", imageUrl: ''),
+  Category(id: "main", title: "Ana Yemek", imageUrl: ''),
+  Category(id: "dessert", title: "Tatlı", imageUrl: ''),
+  Category(id: "snack", title: "Atıştırmalık", imageUrl: ''),
+];
